c7d0dddb45ba4e05891654997c2dffa813475155
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-08-20 15:24:08 +0200

    created user controller

---
+++ src/main/java/com/DataViewerMongoAndSpring/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.DataViewerMongoAndSpring.controller;
+
+import com.DataViewerMongoAndSpring.model.User;
+import com.DataViewerMongoAndSpring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
+}
