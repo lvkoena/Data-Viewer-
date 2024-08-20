0bd389b80c4aa7345004983499e34f731cf92d4b
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-08-20 15:02:44 +0200

    updated model name

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "StructuredData")
-public class DocumentEntity {
+public class User {
 
     @Id
     private String id;
