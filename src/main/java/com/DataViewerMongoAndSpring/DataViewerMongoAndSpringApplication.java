bf55b12b16200ab33824fef1cf0a57c464e12d1c
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-08-20 14:58:48 +0200

    Data Viewer Mongo And Spring Application

---
+++ src/main/java/com/DataViewerMongoAndSpring/DataViewerMongoAndSpringApplication.java
@@ -0,0 +1,13 @@
+package com.DataViewerMongoAndSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataViewerMongoAndSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataViewerMongoAndSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DataViewerMongoAndSpring/DataViewerMongoAndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DataViewerMongoAndSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataViewerMongoAndSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
