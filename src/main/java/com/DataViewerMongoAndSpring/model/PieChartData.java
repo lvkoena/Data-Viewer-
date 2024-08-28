a9807894b8a9e77269e8474f76a8eb38929fadd2
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-08-28 08:23:08 +0200

    updated getters and setters

--- src/main/java/com/DataViewerMongoAndSpring/model/PieChartData.java
+++ src/main/java/com/DataViewerMongoAndSpring/model/PieChartData.java
@@ -19,7 +19,39 @@ public class PieChartData {
         this.value = value;
     }
 
+    public PieChartData(String name, int value, int year) {
+        this.name = name;
+        this.value = value;
+        this.year = year;
+    }
+
     // Default constructor
     public PieChartData() {
+    }
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
