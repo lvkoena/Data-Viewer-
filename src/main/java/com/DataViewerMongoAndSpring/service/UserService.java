33f83b048617ad9951552d526d1b9c099b2c2b3b
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-09-02 09:45:54 +0200

    updated user model

--- src/main/java/com/DataViewerMongoAndSpring/service/UserService.java
+++ src/main/java/com/DataViewerMongoAndSpring/service/UserService.java
@@ -22,27 +22,4 @@ public class UserService {
         User user = repository.findAll().get(0);
         return user.getChartData();
     }
-
-    // Initialize sample data
-    @PostConstruct
-    public void initData() {
-        User user = new User();
-        user.setChartData(List.of(
-                new PieChartData("AWS", 44),
-                new PieChartData("AWS", 43),
-                new PieChartData("Microsoft Azure", 29),
-                new PieChartData("Microsoft Azure", 28),
-                new PieChartData("Google Cloud Platform", 9),
-                new PieChartData("Google Cloud Platform", 11),
-                new PieChartData("Cloudflare", 2),
-                new PieChartData("Cloudflare", 1),
-                new PieChartData("DigitalOcean", 2),
-                new PieChartData("DigitalOcean", 2),
-                new PieChartData("Vercel", 2),
-                new PieChartData("Vercel", 1),
-                new PieChartData("Heroku", 1),
-                new PieChartData("Heroku", 3)
-        ));
-        repository.save(user);
-    }
 }
