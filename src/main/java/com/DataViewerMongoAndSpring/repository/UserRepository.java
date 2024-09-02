6678428dc214bf1a3194e35eb54903c4548c296d
LeboHIndsafri <Lebohang.mokoena@indsafri.com>
2024-09-02 12:28:46 +0200

    updated user model and service

--- src/main/java/com/DataViewerMongoAndSpring/repository/UserRepository.java
+++ src/main/java/com/DataViewerMongoAndSpring/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.DataViewerMongoAndSpring.repository;
 
-import com.DataViewerMongoAndSpring.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
